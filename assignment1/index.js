32b1754b5fa198e8f6cdc274302c1eaaeb3c96e1
anisaberjani1 <anisa.berjani@gmail.com>
2025-06-03 19:36:30 +0200

    Update task1

--- assignment1/index.js
+++ assignment1/index.js
@@ -1,9 +1,9 @@
 //1.Create a simple profile using variables to store a person’s name, age, job, and a fun fact.
 
-const name = "Anisa";
+const fullName = "Anisa";
 let age = 22;
 let job = "Web Developer";
 const funFact = "First class with react";
 
-console.log(`${name} is ${age},and is a ${job}.Here is a fun fact: ${funFact}`)
+console.log(`${fullName} is ${age},and is a ${job}.Here is a fun fact: ${funFact}`)
 
