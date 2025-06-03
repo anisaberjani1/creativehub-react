590d241265f58b2f468acddc798c751244845d0f
anisaberjani1 <anisa.berjani@gmail.com>
2025-06-03 19:53:10 +0200

    second task

--- assignment1/index.js
+++ assignment1/index.js
@@ -7,3 +7,16 @@ const funFact = "First class with react";
 
 console.log(`${fullName} is ${age},and is a ${job}.Here is a fun fact: ${funFact}`)
 
+//2.Write a program that converts temperatures from Celsius to Fahrenheit. 
+//Choose a
+// suitable type of variable for the conversion factor and the Celsius temperature.
+// Display the result of the conversion and then modify the Celsius value to perform
+// another conversion.
+
+
+const celsius = 20;
+
+const fahrenheit = (celsius * 9/5) + 32;
+
+console.log(`${celsius} to convert in fahrenheit is: ${fahrenheit}`)
+
