33c1d5e70c4abf893c9987f781e75825ee936553
anisaberjani1 <anisa.berjani@gmail.com>
2025-06-09 16:03:41 +0200

    Loops Assignments

--- Lecture1/script.js
+++ Lecture1/script.js
@@ -201,5 +201,39 @@ switch (userRole.toLowerCase()) {
         break;
 }
 
+//----------------Looping in Javascript-------------------
+
+// 1. Write a program that prints the numbers from 1 to 10 using a for loop
+for(let i=1; i<=10; i++){
+    console.log(i)
+}
+
+// 2. Create a program that iterates over an array of colors and prints each color to the
+// console using a for loop.
+
+const colors = ['red','black','yellow', 'blue', 'white']
+
+for(let i=0; i<colors.length; i++){
+    console.log(colors[i])
+}
+
+// 3. Write a program that prints a multiplication table for numbers 1 through 5 using
+// nested loops.
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= 5; j++) {
+        console.log(`${i} x ${j} = ${i * j}`);
+    }
+    console.log('');
+}
+
+// 4. Write a program that finds the first number divisible by 7 between 1 and 50. Use a for
+// loop and the break statement to exit the loop as soon as a number is found.
+
+for(let i=1; i<=50; i++){
+    if(i % 7 === 0){
+        console.log(`first number divisible by 7 between 1 and 50 is ${i}`)
+        break;
+    }
+}
 
 
